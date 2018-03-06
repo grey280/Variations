50ad279bfc3ced0a564b7f1cd2f3035570b7bbba
Grey Patterson <greypatterson@gmail.com>
2018-03-06 14:08:46 -0800

    Let's have a cell class

---
+++ mk/Structures/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  Cell.swift
+//  mk
+//
+//  Created by Grey Patterson on 2018-03-06.
+//  Copyright © 2018 Grey Patterson. All rights reserved.
+//
+
+import Foundation
+
+class Cell{
+    /// Whether or not the cell is 'live'
+    var live = false
+}

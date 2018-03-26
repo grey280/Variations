153f5c3c6e46af868cedb91430f7906aba48a1d3
Grey Patterson <greypatterson@gmail.com>
2018-03-26 12:26:04 -0700

    Start developing the ability to display things on a grid

---
+++ mk/Structures/GridView.swift
@@ -0,0 +1,59 @@
+//
+//  GridView.swift
+//  mk
+//
+//  Created by Grey Patterson on 2018-03-26.
+//  Copyright © 2018 Grey Patterson. All rights reserved.
+//
+
+import UIKit
+
+class GridView: UIView{
+    var grid: Grid
+    
+    init(frame: CGRect, grid: Grid) {
+        self.grid = grid
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate var gridWidthMultiple: CGFloat
+    {
+        return 10
+    }
+    fileprivate var gridHeightMultiple : CGFloat
+    {
+        return 20
+    }
+    
+    fileprivate var gridWidth: CGFloat
+    {
+        return bounds.width/CGFloat(gridWidthMultiple)
+    }
+    
+    fileprivate var gridHeight: CGFloat
+    {
+        return bounds.height/CGFloat(gridHeightMultiple)
+    }
+    
+    fileprivate var gridCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    fileprivate func drawGrid()
+    {
+        let path = UIBezierPath()
+        path.lineWidth = 5.0
+        
+        for index in 1...Int(gridWidthMultiple) - 1
+        {
+            let start = CGPoint(x: CGFloat(index) * gridWidth, y: 0)
+            let end = CGPoint(x: CGFloat(index) * gridWidth, y:bounds.height)
+            path.move(to: start)
+            path.addLine(to: end)
+        }
+    }
+}

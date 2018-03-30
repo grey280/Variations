a6c7c130fd00c96bbdf7ea1accf617061efa19af
Grey Patterson <greypatterson@gmail.com>
2018-03-30 13:00:42 -0700

    Well I wrote a single test, that's something, right?

--- mkTests/GridConverterTests.swift
+++ mkTests/GridConverterTests.swift
@@ -10,5 +10,10 @@ import XCTest
 @testable import mk
 
 class GridConverterTests: XCTestCase{
+    let converter = GridConverter()
     
+    func testChordLimiting(){
+        let input1 = [true, false, true, true, false, false]
+        XCTAssertEqual(converter.convert(input1, chord: .I), [51, 58, 63]) // Eb3, Bb3, Eb4
+    }
 }

885522a6f1008c10d5be3a1d2a97fb531bb41876
Grey Patterson <greypatterson@gmail.com>
2018-03-30 10:39:25 -0700

    Start working on converters

---
+++ mk/Structures/GridConverter.swift
@@ -0,0 +1,14 @@
+//
+//  GridConverter.swift
+//  mk
+//
+//  Created by Grey Patterson on 2018-03-30.
+//  Copyright © 2018 Grey Patterson. All rights reserved.
+//
+
+import Foundation
+
+/// Handles the conversions between the various data types we need
+class GridConverter{
+    
+}

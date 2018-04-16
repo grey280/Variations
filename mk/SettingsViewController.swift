3b885c75357ba64d965bf1c95dc56dd7b958c179
Grey Patterson <greypatterson@gmail.com>
2018-04-16 13:43:40 -0700

    Comments and a gesture recognizer

--- mk/SettingsViewController.swift
+++ mk/SettingsViewController.swift
@@ -11,31 +11,24 @@ import UIKit
 /// View controller for handling settings
 class SettingsViewController: UIViewController {
 
+    /// Label showing the duration, in seconds, of each chord
     @IBOutlet weak var durationLabel: UILabel!
+    
+    /// Label showing the number of grids to have; minimum is 2, maximum is as yet undetermined
     @IBOutlet weak var gridsLabel: UILabel!
-    @IBAction func doneButton(_ sender: Any) {
+    /// Handle the 'done' button being pressed
+    ///
+    /// - Parameter sender: ignored
+    @objc @IBAction func doneButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    /// Prep for display; adds gesture recognizer and whatnot
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let swipDownGR = UISwipeGestureRecognizer(target: self, action: #selector(doneButton(_:)))
+        swipDownGR.direction = .down
+        self.view.addGestureRecognizer(swipDownGR)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

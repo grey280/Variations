1ff89a0e940f124fa8e0b472ccd6f901272f6abf
Grey Patterson <greypatterson@gmail.com>
2018-04-16 13:50:41 -0700

    Added comments, removed the key selection for now

--- mk/SettingsViewController.swift
+++ mk/SettingsViewController.swift
@@ -10,12 +10,37 @@ import UIKit
 
 /// View controller for handling settings
 class SettingsViewController: UIViewController {
+    // MARK: Variables
 
     /// Label showing the duration, in seconds, of each chord
     @IBOutlet weak var durationLabel: UILabel!
     
     /// Label showing the number of grids to have; minimum is 2, maximum is as yet undetermined
     @IBOutlet weak var gridsLabel: UILabel!
+    
+    /// Whether or not to use all the chords or just the I IV V I set
+    @IBOutlet weak var chordSwitch: UISwitch!
+    /// Stepper for inputting the duration of each chord
+    @IBOutlet weak var durationStepper: UIStepper!
+    /// Stepper used for inputting the number of grids to use
+    @IBOutlet weak var gridStepper: UIStepper!
+    
+    // MARK: - Functions
+    /// Triggered when the duration stepper is used
+    ///
+    /// - Parameter sender: durationStepper
+    @IBAction func durationStepped(_ sender: UIStepper) {
+    }
+    
+    /// Triggered when the grid stepper is used
+    ///
+    /// - Parameter sender: gridStepper
+    @IBAction func gridStepped(_ sender: UIStepper) {
+    }
+    
+    
+    
+    
     /// Handle the 'done' button being pressed
     ///
     /// - Parameter sender: ignored
@@ -23,7 +48,9 @@ class SettingsViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-    /// Prep for display; adds gesture recognizer and whatnot
+    // MARK: - Setup
+    
+    /// Prep for display; adds gesture recognizer, pull current settings and fill data to be accurate
     override func viewDidLoad() {
         super.viewDidLoad()
         
